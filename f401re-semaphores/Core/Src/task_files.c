3917586fb14f7a4e7cdc8e165b31987791283f98
Jean F. Requejo Rosales <1713jean@gmail.com>
2025-03-12 23:04:54 -0500

    Avance
    Nos detenemos en el uso de la función: assert_failed con asser_param. Aún se evalua.

---
+++ f401re-semaphores/Core/Src/task_files.c
@@ -0,0 +1,48 @@
+/*
+ * task_files.c
+ *
+ *  Created on: Mar 12, 2025
+ *      Author: User
+ */
+
+/*Libraries*/
+#include "main.h"
+#include "cmsis_os.h"
+
+/*Variables*/
+
+/*Function's Prototype*/
+
+void task_main(void *params)
+{
+	for(;;)
+	{
+
+	}
+}
+
+void task_green(void *params)
+{
+	for(;;)
+	{
+
+	}
+}
+
+void task_blue(void *params)
+{
+	for(;;)
+	{
+
+	}
+}
+
+void task_red(void *params)
+{
+	for(;;)
+	{
+
+	}
+}
+
+
